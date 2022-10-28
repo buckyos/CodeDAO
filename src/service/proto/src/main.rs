13a54e33e5681bf9054c56fb15a38e58b801c2c0
liqiang <1344583166@qq.com>
2022-10-28 19:05:28 +0800

    Fix proto main program

--- src/service/proto/src/main.rs
+++ src/service/proto/src/main.rs
@@ -1,23 +1,19 @@
-
-use std::env::set_var;
 use prost_build;
-
+use std::env::set_var;
 
 /// 生成 cyfs_git.rs
-#[async_std::main]
-async fn main() {
+fn main() {
     // cwd  <repo>/services/
     // generate target path
     set_var("OUT_DIR", "cyfs-git-base/src/object_type/proto");
 
     let cwd = std::env::current_dir().unwrap();
-    let proto_root_path = cwd
-        .join("proto");
+    let proto_root_path = cwd.join("proto");
     let proto_file_path = proto_root_path.join("cyfs_git.proto");
     println!("path {:?}", proto_file_path);
 
     let mut config = prost_build::Config::new();
-    config.compile_protos(
-        &[proto_file_path],
-        &[proto_root_path]).unwrap();
+    config
+        .compile_protos(&[proto_file_path], &[proto_root_path])
+        .unwrap();
 }
